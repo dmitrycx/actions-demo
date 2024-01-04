5de6e113c8d82a1c46981a6dd4b9f9d022f7c2d5
dmitrycx <dkolotsey@gmail.com>
2024-01-05 00:31:03 +0300

    comment out failing demo test

--- blazorapp/tests/UnitTests/UnitTest1.cs
+++ blazorapp/tests/UnitTests/UnitTest1.cs
@@ -2,11 +2,12 @@ namespace UnitTests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void ShouldFail()
-    {
-        Assert.True(false);
-    }
+    // uncomment before demo
+    // [Fact]
+    // public void ShouldFail()
+    // {
+    //     Assert.True(false);
+    // }
     
     [Fact]
     public void ShouldPass()
