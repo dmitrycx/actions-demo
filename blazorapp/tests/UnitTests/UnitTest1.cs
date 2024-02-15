6fe9cd8a1acf70233f80229112696b2b0241d926
Dmitry Kolotsey <dkolotsey@gmail.com>
2024-02-15 16:41:02 +0300

    Update UnitTest1.cs

--- blazorapp/tests/UnitTests/UnitTest1.cs
+++ blazorapp/tests/UnitTests/UnitTest1.cs
@@ -3,11 +3,11 @@ namespace UnitTests;
 public class UnitTest1
 {
     // uncomment before demo
-    [Fact]
-    public void ShouldFail()
-    {
-        Assert.True(false);
-    }
+    // [Fact]
+    // public void ShouldFail()
+    // {
+    //     Assert.True(false);
+    // }
     
     [Fact]
     public void ShouldPass()
